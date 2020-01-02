6b2f9979492f7b71c8db2adb550ade66f331db36
Luiz Américo <luizamericop@gmail.com>
2020-01-02 16:31:31 -0300

    Do not use babel on build

--- tools/build.js
+++ tools/build.js
@@ -2,12 +2,11 @@
 
 const del = require('del')
 const rollup = require('rollup')
-const babel = require('rollup-plugin-babel')
 const pkg = require('../package.json')
 
 let promise = Promise.resolve()
 
-let dependencies = Object.assign({}, pkg.dependencies || {}, pkg.peerDependencies || {})
+const dependencies = Object.assign({}, pkg.dependencies || {}, pkg.peerDependencies || {})
 
 // Clean up the output directory
 promise = promise.then(() => del(['dist/*']))
@@ -16,13 +15,9 @@ promise = promise.then(() => del(['dist/*']))
 for (const format of ['es']) {
   promise = promise.then(() => rollup.rollup({
     input: 'src/index.js',
-    external: ['underscore', 'nextbone/dom-utils', ...Object.keys(dependencies)],
-    plugins: [babel({
-      exclude: 'node_modules/**',
-      sourceMaps: true
-    })]
+    external: ['underscore', 'nextbone/dom-utils', 'slick-router/middlewares/router-links', ...Object.keys(dependencies)]
   }).then(bundle => bundle.write({
-    file: `dist/nextbone-routing.js`,
+    file: 'dist/nextbone-routing.js',
     format,
     sourcemap: true
   })))
